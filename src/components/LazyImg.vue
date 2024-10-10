3c845d677888a93b65c284db6f36ad7fdb5220dd
Anton Chyzhyk <antonchizhik2000@gmail.com>
2024-10-10 14:06:25 +0300

    Created a LazyImg.vue component

---
+++ src/components/LazyImg.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    ref="intersectionTargetRef"
+    class="w-full h-full bg-gray-ultra-light relative"
+    :class="{'flex items-center justify-center': imageHasError}"
+  >
+    <el-skeleton v-if="isSkeletonLoading" class="w-full h-full absolute" animated>
+      <template #template>
+        <el-skeleton-item class="w-full h-full absolute" />
+      </template>
+    </el-skeleton>
+
+    <img
+      v-if="observedImgSrc && imageLoadingStarted"
+      class="object-cover w-full h-full absolute"
+      :src="observedImgSrc"
+      :alt="imgAlt"
+      @load="onLoad"
+      @error="onError"
+    >
+
+    <slot v-if="imageHasError" name="placeholder">
+      <IconImage class="w-[20px] h-[20px] bg-gray" />
+    </slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const props = defineProps<{
+  imgSrc: string
+  imgAlt: string
+}>()
+
+const intersectionTargetRef = ref<HTMLElement | null>(null)
+const imageHasError = ref(false)
+const isSkeletonLoading = ref(true)
+const observedImgSrc = ref<string | null>(null)
+const imageLoadingStarted = ref(false)
+const isLoaded = ref(false)
+
+const onLoad = () => {
+  isLoaded.value = true
+  imageHasError.value = false
+  isSkeletonLoading.value = false
+  imageLoadingStarted.value = true
+}
+
+const onError = () => {
+  imageHasError.value = true
+  isSkeletonLoading.value = false
+  imageLoadingStarted.value = false
+}
+
+let observer: IntersectionObserver | null = null
+
+const observeImage = () => {
+  observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        imageLoadingStarted.value = true
+        observedImgSrc.value = props.imgSrc
+        observer?.unobserve(entry.target)
+      }
+    },
+    {
+      rootMargin: '300px'
+    }
+  )
+  if (intersectionTargetRef.value) {
+    observer.observe(intersectionTargetRef.value)
+  }
+}
+
+onMounted(() => {
+  observeImage()
+})
+
+onUnmounted(() => {
+  if (observer && intersectionTargetRef.value) {
+    observer.unobserve(intersectionTargetRef.value)
+  }
+})
+</script>
